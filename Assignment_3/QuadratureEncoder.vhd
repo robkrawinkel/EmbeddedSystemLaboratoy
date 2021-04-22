349a26c8c3554f4c67376424420621bfd43fe19b
Rob Krawinkel <r.krawinkel@solarteam.nl>
2021-04-22 12:03:17 +0200

    Implemented quadrature encoder

--- Assignment_3/QuadratureEncoder.vhd
+++ Assignment_3/QuadratureEncoder.vhd
@@ -12,76 +12,128 @@ ENTITY QuadratureEncoder IS
 	signalA	: IN std_logic;
 	signalB	: IN std_logic;
 	
-	-- Output velocity in 16 bits
-	velocity	: OUT std_logic_vector(15 DOWNTO 0);
+	-- Output velocity in 32 bits
+	velocity	: OUT integer;
+	
+	-- Output error
+	overSpeedError : OUT std_logic
 
 	);
 END ENTITY;
 
 ARCHITECTURE bhv OF QuadratureEncoder IS
-
-	-- Create a timer object from the timer entity
-	COMPONENT timer IS
-		PORT (reset		: IN std_logic;
-				CLOCK 	: IN std_logic;
-				runTimer : IN std_logic;
-				result 	: INOUT integer
-				);
-	END COMPONENT timer;
 	
-	-- Create variables to manage the timer
-	VARIABLE runTimer		: std_logic;
-	VARIABLE resetTimer	: std_logic;
-	VARIABLE timerCount	: integer;
-	
-	-- Create variable to keep track of direction
-	VARIABLE CW	: std_logic;
+	-- Create signals to manage the timer
+	SIGNAL runTimer	: std_logic;
+	SIGNAL resetTimer	: std_logic;
+	SIGNAL timerCount	: integer;
+	SIGNAL maxTimer	: integer := 50000000;
+	SIGNAL timerLimit	: std_logic;	
 
 BEGIN
-
-	-- Start the timer tied to the created variables
-	timer	: timer	PORT MAP (resetTimer, CLOCK_50, runTimer, timerCount);
+	
+	-- Initialize timer and tie to local variables and signals
+	timer : ENTITY work.timer
+		PORT MAP (
+			reset		=> resetTimer,
+			CLOCK 	=> CLOCK_50,
+			runTimer => runTimer,
+			maxTimer => maxTimer,
+			result 	=> timerCount,
+			timerLimit => timerLimit
+		);
 
 	-- Create a process which reacts to the specified signals
-	PROCESS(reset, CLOCK_50, signalA, signalB)
+	PROCESS(reset, CLOCK_50, signalA, signalB, timerLimit)
 	
+		-- Create variable to keep track of direction, ClockWise
+		VARIABLE CW	: std_logic;
+		
+		-- Variables to keep track of previous states
+		VARIABLE oldStateA : std_logic;
+		VARIABLE oldStateB : std_logic;
+		
+		-- Define device constants
 		CONSTANT RadPerPulse : integer := 12566;	-- 2pi/pulsesPerRot*1000000, with 500 pulsesPerRot
-		CONSTANT minRotSpeed : integer := 0;		-- Defined in clock pulses
-		CONSTANT maxRotSpeed : integer := 1000;	-- Defined in clock pulses
+		CONSTANT maxRotSpeed : integer := 1;		-- Defined in clock pulses
 		
 	BEGIN
 		
 		-- Reset everything
 		IF reset = '0' THEN
 			velocity <= 0;
-			runTimer := '0';
-			resetTimer := '0';
+			runTimer <= '0';
+			resetTimer <= '0';
+			overSpeedError <= '0';
 			
-		ELSIF rising_edge(signalA) THEN
-			IF resetTimer == '0' THEN	-- If the timer is not running yet
-				resetTimer == '1';		-- Release the timer from reset
-				runTimer == '1';			-- Start counting
-				CW == '1';					-- Note that the rotation is CW
+		-- If A is detected
+		ELSIF rising_edge(CLOCK_50) THEN
+			
+			IF (signalA /= oldStateA AND signalA = '1') THEN
+				IF resetTimer = '0' THEN	-- If the timer is not running yet
+					resetTimer <= '1';			-- Release the timer from reset
+					runTimer <= '1';			-- Start counting
+					CW := '1';					-- Note that the rotation is CW
+					
+				ELSIF CW = '0'	THEN			-- If the timer is running and rotation is CCW
+					runTimer <= '0';			-- Stop the timer
+					
+					IF timerCount > maxRotSpeed THEN		-- If not too fast
+						velocity <= -1000000*(RadPerPulse*(50000000/1000000))/(timerCount);  -- in micro Rad / s
+						overSpeedError <= '0';
+						
+					ELSE 											-- If too fast
+						-- Don't alter velocity
+						overSpeedError <= '1';
+						
+					END IF;
+					
+				END IF;
 				
-			ELSIF CW == '0'				-- If the timer is running and rotation is CCW
-				runTimer == '0';			-- Stop the timer
-				IF (timerCount > minRotSpeed and timerCount < maxRotSpeed)
-					velocity <= (RadPerPulse/1000000)/(timerCount/50000000);
+			
+		
+			-- If B is detected
+			ELSIF (signalB /= oldStateB AND signalB = '1') THEN
+			
+				IF resetTimer = '0' THEN	-- If the timer is not running yet
+					resetTimer <= '1';			-- Release the timer from reset
+					runTimer <= '1';			-- Start counting
+					CW := '0';					-- Note that the rotation is CW
+					
+				ELSIF CW = '1' THEN				-- If the timer is running and rotation is CCW
+					runTimer <= '0';			-- Stop the timer
+					
+					IF timerCount > maxRotSpeed THEN		-- If not too fast
+						velocity <= 1000000*(RadPerPulse*(50000000/1000000))/(timerCount);  -- in micro Rad / s
+						overSpeedError <= '0';
+						
+					ELSE 											-- If too fast
+						-- Don't alter velocity
+						overSpeedError <= '1';
+						
+					END IF;
+					
+				END IF;
+			
+			
+		
+			-- Reset loop
+			ELSIF ((signalA /= oldStateA AND signalA = '0') OR (signalB /= oldStateB AND signalB = '0')) THEN
+				resetTimer <= '0';
+				runTimer <= '0';
+			
+			
+			-- Timer overflow
+			ELSIF timerLimit = '1' THEN
+				velocity <= 0;
+				resetTimer <= '0';
+				runTimer <= '0';
 				
 			END IF;
-		
-		ELSIF rising_edge(signalB) THEN
-		
-		ELSIF falling_edge(signalA) THEN
-			runTimer := '0';
-			resetTimer := '0';
-			velocity <= 0;
-		
-		ELSIF falling_edge(signalB) THEN
-			runTimer := '0';
-			resetTimer := '0';
-			velocity <= 0;
-				
+			
+			oldStateA := signalA;
+			oldStateB := signalB;
+			
 		END IF;
 		
 	END PROCESS;
