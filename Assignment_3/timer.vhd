be5c4d8e64e232116c2d23e96863cf4b0a01462c
Rob Krawinkel <r.krawinkel@solarteam.nl>
2021-04-22 12:02:53 +0200

    Added timer limit

--- Assignment_3/timer.vhd
+++ Assignment_3/timer.vhd
@@ -1,13 +1,14 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
-USE work.common.ALL;
 
 ENTITY timer IS
 	PORT (reset		: IN std_logic;
 			CLOCK 	: IN std_logic;
 			runTimer : IN std_logic;
-			result 	: OUT integer
+			maxTimer : IN integer;
+			result 	: INOUT integer;
+			timerLimit : OUT std_logic
 			);
 END timer;
 
@@ -24,11 +25,17 @@ BEGIN
 	
 		IF reset = '0' THEN
 			result <= 0;
+			timerLimit <= '0';
 			
 		ELSIF runTimer = '1' THEN
 			
 			IF rising_edge(CLOCK) THEN
+				IF result < maxTimer THEN
 					result <= result + 1;
+				ELSE
+					result <= maxTimer;
+					timerLimit <= '1';
+				END IF;
 
 			END IF;
 				
