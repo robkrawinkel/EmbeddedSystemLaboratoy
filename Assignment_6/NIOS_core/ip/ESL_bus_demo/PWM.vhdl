ff838658eac5dd64f89e21be3ab8100554f13af9
robkrawinkel <rob@kraewinkel.nl>
2021-05-09 16:37:57 +0200

    Added comments to PWM

--- Assignment_6/NIOS_core/ip/ESL_bus_demo/PWM.vhdl
+++ Assignment_6/NIOS_core/ip/ESL_bus_demo/PWM.vhdl
@@ -9,23 +9,24 @@ ENTITY PWM IS
 	CLOCK_50	: IN std_logic;
 
 	-- Input signals module
-	frequency	: IN integer range 0 to 50000000; --frequency of the signal in Hz
-	dutycycle	: IN integer range 0 to 100; --dutycycle of the signal in percentage
-	CW			: IN std_logic; --rotational direction of the signal
-	
+	frequency	: IN integer range 0 to 50000000; 	--frequency of the signal in Hz
+	dutycycle	: IN integer range 0 to 100; 		--dutycycle of the signal in percentage
+	CW		: IN std_logic; 			--rotational direction of the signal
 
 	-- Output pwm_signal and rotation direction
 	PWM_signal 	: OUT std_logic;
 	INA 		: OUT std_logic;
-	INB			: OUT std_logic;
-
-    enable      : IN std_logic
+	INB		: OUT std_logic;
+	
+	-- Enable for the PMW module
+    	enable      	: IN std_logic
 
 	);
 END ENTITY;
 
 ARCHITECTURE bhv OF PWM IS
-
+	
+	-- Counter for the amount of clock cycles
 	SIGNAL counter : integer range 0 to 50000000;
 
 BEGIN
@@ -34,6 +35,7 @@ BEGIN
 	-- Create a process which reacts to the specified signals
 	PROCESS(reset, CLOCK_50)
 		
+		-- Variables that store the amount of clock cycles to count based on the set frequency and dutycycle
 		VARIABLE cycles_per_period : integer range 0 to 50000000;
 		VARIABLE on_cycles_per_period : integer range 0 to 50000000;
 		
@@ -50,40 +52,39 @@ BEGIN
 			
 		ELSIF rising_edge(CLOCK_50) THEN
 
-			--calculate the relevant variables at the start of each PWM cycle
+			-- calculate the relevant variables at the start of each PWM cycle
 			cycles_per_period := 50000000/frequency;
 			on_cycles_per_period := ((50000000/frequency) * dutycycle) / 100;
 
 			--set the rotational direction based on the CW input
-            IF enable = '1' THEN
+            		IF enable = '1' THEN
 
 				-- Set the directional outputs
-			    INA <= CW;
+				INA <= CW;
 			    INB <= NOT CW;
             
-
                 --change the PWM signal depending on the counter
-                IF counter = 0 THEN
-                    PWM_signal <= '1';
+				IF counter = 0 THEN
+					PWM_signal <= '1';
 					counter <= counter + 1;
 
-                ELSIF counter = on_cycles_per_period-1 THEN
-                    PWM_signal <= '0';
+				ELSIF counter = on_cycles_per_period-1 THEN
+					PWM_signal <= '0';
 					counter <= counter + 1;
 
-                ELSIF counter = cycles_per_period-1 THEN
-                    PWM_signal <= '1';
-                    counter <= 0;
-                ELSE
-                    counter <= counter + 1;
-                END IF;
+				ELSIF counter = cycles_per_period-1 THEN
+					PWM_signal <= '1';
+					counter <= 0;
+				ELSE
+					counter <= counter + 1;
+				END IF;
 
-            ELSE
-                INA <= '0';
-                INB <= '0';
-                PWM_signal <= '0';
-                counter <= 0;
-            END IF;
+			ELSE -- If the PWM module is not enabled
+				INA <= '0';
+				INB <= '0';
+				PWM_signal <= '0';
+				counter <= 0;
+			END IF;
 			
 		END IF;
 		
