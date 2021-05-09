a56889c8df7f055c5e1cb7ea918d779cced00335
robkrawinkel <rob@kraewinkel.nl>
2021-05-09 16:41:17 +0200

    Added comments and repaired timer

--- Assignment_6/NIOS_core/ip/ESL_bus_demo/timer.vhdl
+++ Assignment_6/NIOS_core/ip/ESL_bus_demo/timer.vhdl
@@ -3,10 +3,10 @@ USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 
 ENTITY timer IS
-	PORT (reset		: IN std_logic;
-			CLOCK 	: IN std_logic;
-			runTimer : IN std_logic;
-			result 	: INOUT integer;
+	PORT (reset			: IN std_logic;
+			CLOCK 		: IN std_logic;
+			runTimer 	: IN std_logic;
+			result 		: INOUT integer;
 			);
 END timer;
 
@@ -17,22 +17,21 @@ BEGIN
 
 	PROCESS(reset, CLOCK, runTimer)
 	
-		
-		
 	BEGIN
-	
+
+		-- If timer is reset
 		IF reset = '1' THEN
 			result <= 0;
 			timerLimit <= '0';
 			
+		-- If timer is enabled
 		ELSIF runTimer = '1' THEN
 			
+			-- On every clock cycle, increase the counter
 			IF rising_edge(CLOCK) THEN
 				result <= result + 1;
-				END IF;
-
 			END IF;
-				
+
 		END IF;
 		
 	END PROCESS;
