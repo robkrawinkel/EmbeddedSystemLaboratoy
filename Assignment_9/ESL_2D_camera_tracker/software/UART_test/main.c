45bed00515549aeee7e43cd30ef584543ef6db05
jjackpack <vriesdehidde@gmail.com>
2021-05-27 11:42:39 +0200

    crying

--- Assignment_9/ESL_2D_camera_tracker/software/UART_test/main.c
+++ Assignment_9/ESL_2D_camera_tracker/software/UART_test/main.c
@@ -13,7 +13,7 @@
 // for more information.
 //
 //-----------------------------------------------------------------------------
-
+/*
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -26,10 +26,13 @@
 #include "sys/alt_irq.h"
 
 #include "sys/alt_stdio.h"
+
 #include "altera_avalon_uart_regs.h"
-#include "system.h"
+#include "altera_avalon_uart.h"
+#include "altera_avalon_uart_fd.h"
+
+
 #include "alt_types.h"
-#include "sys/alt_irq.h"
 
 
 unsigned char RX;
@@ -38,6 +41,7 @@ unsigned char TX;
 
 
 void UARTReceive(void* context) {
+
 	RX = IORD_ALTERA_AVALON_UART_RXDATA(UART_0_BASE);
 	//RX = IORD(UART_0_BASE, 0);
 	printf("message received\n");
@@ -50,7 +54,7 @@ void UARTReceive(void* context) {
 int main()
 {
 	// Say hello through the debug interface
-	printf("Hello from Nios II!\n");
+
 
 	// Put 0x08 in the memory of the IP and enable the count down
 	// IOWR(ESL_BUS_DEMO_0_BASE, 0x00, 1 << 31 | 0x08);
@@ -60,15 +64,9 @@ int main()
 	int32_t nReadOut = 0;
 	int32_t stepCount0 = 0;
 	int32_t stepCount1 = 0;
-	
-
 
 	//setup for serial communication
-    alt_ic_isr_register(UART_0_IRQ_INTERRUPT_CONTROLLER_ID,
-                        UART_0_IRQ,
-                        UARTReceive,
-                        NULL,
-                        NULL);
+    alt_ic_isr_register(UART_0_IRQ_INTERRUPT_CONTROLLER_ID, UART_0_IRQ, UARTReceive, NULL, NULL);
 
 	//IOWR_ALTERA_AVALON_UART_STATUS(UART_0_BASE, 0x0); 					//Clear status register
 	//IORD_ALTERA_AVALON_UART_RXDATA(UART_0_BASE); 						//Read the useless value in the empty receive register
@@ -77,6 +75,9 @@ int main()
 
 	alt_ic_irq_enable(UART_0_IRQ_INTERRUPT_CONTROLLER_ID, UART_0_IRQ);
 
+	IOWR(UART_0_BASE,1,12);
+
+	printf("Hello from Nios II!\n");
 	// Now loop forever ...
 	while (1) {
 		nReadOut = IORD(ESL_NIOS_II_IP_0_BASE, 0x00);
@@ -89,8 +90,11 @@ int main()
 		
 		//printf("stepCount0: %d\t stepCount1: %d \n\r", stepCount0, stepCount1);
 
+		IOWR(UART_0_BASE,1,'A');
+		usleep(1000000);
+
 		if (RXReceived == 1) {
-			TX = RX;
+			TX = RX;UART_0_IRQ_INTERRUPT_CONTROLLER_ID,
 			IOWR(UART_0_BASE, 1, TX);
 			RXReceived = 0;
 		}
@@ -100,4 +104,62 @@ int main()
 
 
 	return 0;
+}*/
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <alt_types.h>
+#include <io.h>
+#include "system.h"
+#include "sys/alt_irq.h"
+#include "InterruptHandlerForUart.h"
+
+#define BAUD_RATE_0 115200
+#define BAUD_RATE_1 115200
+
+void InitUart()
+{
+    int context_uart1,context_uart2;
+
+    InitUart1(BAUD_RATE_0);
+    //InitUart2(BAUD_RATE_1);
+
+    //alt_ic_isr_register(UART_0_IRQ_INTERRUPT_CONTROLLER_ID,UART_0_IRQ,&context_uart1,IsrUart1,NULL); // install UART1 ISR
+    alt_irq_register(UART_0_IRQ,&context_uart1,IsrUart1 ); // install UART1 ISR
+
+    //alt_ic_irq_enable (UART_0_IRQ_INTERRUPT_CONTROLLER_ID,UART_0_IRQ);
+    alt_irq_enable (UART_0_IRQ);
+}
+
+
+int main()
+{
+unsigned char ch;
+printf("\n\nHello NiosII!!!!!\n");
+
+InitUart();
+
+while(1) {
+	//IOWR_ALTERA_AVALON_UART_CONTROL(UART_0_BASE, 'A');
+	PutUart1('a');
+	usleep(10000);
+	if(!EmptyUart1()){
+		printf("received message");
+		ch = GetUart1();
+		PutUart1(ch);
+	}
+	/*
+    if(!EmptyUart1()) {
+        ch = GetUart1();
+        PutUart2(ch);
+    }
+    if(!EmptyUart2()) {
+        ch = GetUart2();
+        PutUart1(ch);
+    }*/
+} //while
+
+return 0;
+
 }
