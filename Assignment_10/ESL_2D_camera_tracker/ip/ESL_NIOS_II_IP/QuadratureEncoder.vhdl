b21c5c92cd0143d2b7e0d1454e18cd19c1fa9294
robkrawinkel <rob@kraewinkel.nl>
2021-06-06 12:38:38 +0200

    Added min and max to stepCount in the quadrature encoder part

--- Assignment_10/ESL_2D_camera_tracker/ip/ESL_NIOS_II_IP/QuadratureEncoder.vhdl
+++ Assignment_10/ESL_2D_camera_tracker/ip/ESL_NIOS_II_IP/QuadratureEncoder.vhdl
@@ -30,6 +30,10 @@ ENTITY QuadratureEncoder IS
 	-- Output step counter in 32 bits signed
 	stepCount 	: INOUT integer;
 
+	-- Input stepCount min and max value
+	stepCount_min	: IN integer RANGE -8192 TO 0;
+	stepCount_max	: IN integer RANGE 0 TO 8191;
+
 	--Reset stepcount to 0
 	stepReset : IN std_logic
 
@@ -50,6 +54,7 @@ BEGIN
 		
 		-- Variables to keep track of previous states
 		VARIABLE oldState : integer range 0 to 4;
+
 	BEGIN
 		
 		-- Reset everything
@@ -93,18 +98,18 @@ BEGIN
 						CW := '1';
 					ELSIF state = 0 AND oldState = 3 THEN
 						CW := '1';
-						stepCount <= stepCount + 1;
+						stepCount <= minimum(stepCount + 1, stepCount_max);
 					ELSIF state = oldState - 1 THEN
 						CW := '0';
 					ELSIF state = 3 AND oldState = 0 THEN
 						CW := '0';
-						stepCount <= stepCount - 1;
+						stepCount <= maximum(stepCount - 1, stepCount_min);
 					ELSE
 						-- if it is not an increase or decrease of one step, assume the rotational direction didn't change and check if the counter should be increased
 						IF state < oldState AND CW = '1' THEN
-							stepCount <= stepCount + 1;
+							stepCount <= minimum(stepCount + 1, stepCount_max);
 						ELSIF state > oldState AND CW = '0' THEN
-							stepCount <= stepCount - 1;
+							stepCount <= maximum(stepCount - 1, stepCount_min);
 						END IF;
 					END IF;
 				END IF;
