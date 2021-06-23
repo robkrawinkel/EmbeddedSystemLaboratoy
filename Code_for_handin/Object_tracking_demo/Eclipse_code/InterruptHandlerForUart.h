aa44e8654c1d9fbf5c62457c5781bfba27770bd9
Hidde de Vries <vriesdehidde@gmail.com>
2021-06-23 14:34:55 +0200

    added handin folder

---
+++ Code_for_handin/Object_tracking_demo/Eclipse_code/InterruptHandlerForUart.h
@@ -0,0 +1,27 @@
+//-----------------------------------------------------------------------------
+//Description : Uart Interrupt Handler & Q Buffer For NiosII
+//Vision : V1.0
+//Filename : InterruptHandlerForUart.h 
+// Copyright 2006, Cheong Min LEE
+// Email: lcm2559@yahoo.co.kr
+// The test may be run in NiosII standalone mode
+//-----------------------------------------------------------------------------
+ 
+#ifndef _INTERRUPTHANDLERFORUART_H_
+#define _INTERRUPTHANDLERFORUART_H_
+ 
+/************************************************** ***************************
+* Public function prototypes
+************************************************** **************************/
+void InitUart0(unsigned int BaudRate);
+
+void IsrUart0();
+
+unsigned char EmptyUart0();
+
+unsigned char GetUart0(void);
+
+unsigned char PutUart0(unsigned char in_char);
+
+ 
+#endif //_INTERRUPTHANDLERFORUART_H_
